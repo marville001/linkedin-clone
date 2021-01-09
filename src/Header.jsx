ab01d7bf2f867743f1b57ab998df163a470e2faa
marville001 <mwangimartin1904@gmail.com>
2021-01-09 12:58:51 +0300

    First commit

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css'
+// import SearchIcon from '@material-ui/icons/Search'
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header_left">
+                <img src="" alt="" />
+                <div className="header_search">
+                    {/* <SearchIcon/> */}
+                    <input type="text" name="" id=""/>
+                </div>
+            </div>
+            <div className="header_right"></div>
+        </div>
+    )
+}
+
+export default Header
